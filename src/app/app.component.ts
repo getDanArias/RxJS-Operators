54938c011fc031e2d2572127e069831f58cb9048
Decahub <dev@decamail.com>
2018-02-08 13:23:49 -0500

    Change title in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <h1>{{title}}</h1>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Data Treatment Plant';
 }
