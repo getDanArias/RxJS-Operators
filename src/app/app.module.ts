b0884577d4a2bee63ca2fa9c0f0dca2ef18359c5
Decahub <dev@decamail.com>
2018-02-08 13:44:25 -0500

    Scaffold Operators component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { OperatorsComponent } from './features/operators/operators/operators.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OperatorsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/features/operators/operators/operators.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-operators',
+  template: `
+    <p>
+      Operators Component
+    </p>
+  `,
+  styleUrls: ['./operators.component.scss']
+})
+export class OperatorsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
