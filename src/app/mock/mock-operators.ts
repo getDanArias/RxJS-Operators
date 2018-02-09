350129ee577dfabe7eaeafd26370bb834eb1f239
Decahub <dev@decamail.com>
2018-02-09 18:18:09 -0500

    Create mock operators.

---
+++ src/app/mock/mock-operators.ts
@@ -0,0 +1,38 @@
+import { Operator } from '../shared/models/operator';
+
+const categories = {
+  creating: `Creating`,
+  transforming: `Transforming`,
+  filtering: `Filtering`,
+  combining: `Combining`,
+  errorHandling: `Error Handling`,
+  utility: `Utility`,
+  conditionalBoolean: `Conditional and Boolean`,
+  mathematicalAggregate: `Mathematical and Aggregate`,
+  backpressure: `Backpressure`,
+  connectable: `Connectable`,
+  converting: `Converting`
+};
+
+export const OPERATORS: Operator[] = [
+  {
+    name: `Create`,
+    category: categories.creating,
+    description: `create an Observable from scratch by calling observer methods programmatically`
+  },
+  {
+    name: `From`,
+    category: categories.creating,
+    description: `convert some other object or data structure into an Observable`
+  },
+  {
+    name: `Scan`,
+    category: categories.transforming,
+    description: `apply a function to each item emitted by an Observable, sequentially, and emit each successive value`
+  },
+  {
+    name: `Filter`,
+    category: categories.filtering,
+    description: `emit only those items from an Observable that pass a predicate test`
+  },
+];
